923c4a64b67bab574983ede0bb27d64163e596dd
nwaszkowiak <norbert.waszkowiak@gmail.com>
2023-03-29 08:48:34 +0200

    Initial commit

---
+++ mail.py
@@ -0,0 +1,17 @@
+import re
+
+class EmailExtractor:
+
+    def __int__(self, emailD):
+        self.email = emailD
+
+    def is_student(self) -> bool:
+        x = re.compile("", )
+
+        return False
+
+    def get_name(self) -> str:
+        return "Norbert"
+
+    def get_surname(self) -> str:
+        return "Waszkowiak"
